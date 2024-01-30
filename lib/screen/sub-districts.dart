8aedb56ad0e5c6aae2a457d285475c4aca29ba2b
Manel-Rejeb <manelrejeb711@gmail.com>
2024-01-30 23:27:01 +0100

    fixing sub-district screen

--- lib/screen/sub-districts.dart
+++ lib/screen/sub-districts.dart
@@ -1,6 +1,7 @@
-import 'dart:convert';
+import 'package:crm/cron/app-district-notifier.dart';
+import 'package:crm/model/district.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 class SubDistricts extends StatefulWidget {
   final String districtName;
@@ -9,51 +10,32 @@ class SubDistricts extends StatefulWidget {
   SubDistricts({required this.districtName, required this.districtId});
 
   @override
-  _SubDistrictState createState() => _SubDistrictState();
+  State<SubDistricts> createState() => _SubDistrictsState();
 }
 
-class _SubDistrictState extends State<SubDistricts> {
-  List<dynamic> subDistrictData = [];
-
+class _SubDistrictsState extends State<SubDistricts> {
   @override
   void initState() {
     super.initState();
-    fetchData();
-  }
-
-  Future<void> fetchData() async {
-    final response = await http.get(
-        Uri.parse('https://65b2ee529bfb12f6eafe8e75.mockapi.io/disctricts/'));
-
-    if (response.statusCode == 200) {
-      setState(() {
-        subDistrictData = json.decode(response.body);
-      });
-    } else {
-      throw Exception('Failed to load sub-district data');
-    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('SubDistrict - ${widget.districtName}'),
-      ),
-      body: Center(
-        child: subDistrictData.isEmpty
-            ? CircularProgressIndicator()
-            : ListView.builder(
-                itemCount: subDistrictData.length,
-                itemBuilder: (context, index) {
-                  final subDistrict = subDistrictData[index];
-                  return ListTile(
-                    title: Text(subDistrict['subDistrictName']),
-                    subtitle: Text('ID: ${subDistrict['subDistrictId']}'),
-                  );
-                },
-              ),
-      ),
-    );
+    return Consumer<AppDistrictNotifier>(builder: (context, state, child) {
+      Future<List<District>> districts = state.districts;
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Bezirk ${widget.districtName}'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(10),
+          width: double.infinity,
+          height: double.infinity,
+          child: const Center(
+            child: Text('SubDistricts Page Content'),
+          ),
+        ),
+      );
+    });
   }
 }
