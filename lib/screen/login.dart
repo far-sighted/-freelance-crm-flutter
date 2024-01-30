534726ccaa1c0608cd20ab535932a686e4b79ec6
Walee Sebiiy <waleesebiiy@gmail.com>
2024-01-30 23:59:11 +0100

    update login page

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -26,7 +26,7 @@ class _LoginState extends State<Login> {
               height: 200.0,
               width: MediaQuery.of(context).size.width,
               padding:
-                  const EdgeInsets.only(left: 24.0, right: 24.0, bottom: 24.0),
+                  const EdgeInsets.all(10.0),
               color: Theme.of(context).colorScheme.surface,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
@@ -52,7 +52,7 @@ class _LoginState extends State<Login> {
             ),
             const Divider(),
             Container(
-              padding: const EdgeInsets.all(24.0),
+              padding: const EdgeInsets.all(10.0),
               color: Theme.of(context).colorScheme.background,
               child: Form(
                 key: key,
