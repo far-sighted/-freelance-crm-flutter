454c72832a4c094d186e103390d4a07c31635ba3
Manel-Rejeb <manelrejeb711@gmail.com>
2024-01-31 20:43:04 +0100

    create routing to clinet list page

--- lib/screen/client-list.dart
+++ lib/screen/client-list.dart
@@ -3,7 +3,10 @@ import 'package:crm/screen/client-form.dart';
 import 'package:crm/component/client-floating-add-button.dart';
 
 class ClientList extends StatefulWidget {
-  const ClientList({super.key});
+  final String id;
+  final String zone;
+
+  const ClientList({super.key, required this.id, required this.zone});
 
   @override
   State<ClientList> createState() => _ClientListState();
@@ -14,7 +17,7 @@ class _ClientListState extends State<ClientList> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Client List'),
+        title: Text('${widget.zone} Clients'),
       ),
       floatingActionButton: const FloatingButton(path: ClientForm()),
     );
--- lib/screen/sub-districts.dart
+++ lib/screen/sub-districts.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:crm/model/sub-district.dart';
 import 'package:crm/component/district-list-item.dart';
 import 'package:crm/service/sub-district.service.dart';
-
+import 'package:crm/screen/client-list.dart';
 
 class SubDistricts extends StatefulWidget {
   final String districtName;
@@ -27,51 +27,46 @@ class _SubDistrictsState extends State<SubDistricts> {
 
   @override
   Widget build(BuildContext context) {
-
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Bezirk ${widget.districtName}'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bezirk ${widget.districtName}'),
+      ),
       drawer: const SideDrawer(),
-        body: Container(
-          padding: const EdgeInsets.all(10),
-          width: double.infinity,
-          height: double.infinity,
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        width: double.infinity,
+        height: double.infinity,
         child: FutureBuilder<List<SubDistrict>>(
             future: _subDistrictsFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else {
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else {
                 List<SubDistrict> subDistricts = snapshot.data ?? [];
-                  return GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 1,
-                      crossAxisSpacing: 4.0,
-                      childAspectRatio: 5 / 1,
-                      mainAxisSpacing: 4.0,
-                    ),
+                return GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1,
+                    crossAxisSpacing: 4.0,
+                    childAspectRatio: 5 / 1,
+                    mainAxisSpacing: 4.0,
+                  ),
                   itemCount: subDistricts.length,
-                    itemBuilder: (context, index) => GestureDetector(
+                  itemBuilder: (context, index) => GestureDetector(
                       onTap: () {
-                        Navigator.push(
-                          context,
+                        Navigator.of(context).push(
                           MaterialPageRoute(
-                              builder: (context) => SubDistricts(
-                                  districtName: subDistricts[index].zone,
-                                  districtId: subDistricts[index].id)),
+                              builder: (context) => ClientList(
+                                  zone: subDistricts[index].zone,
+                                  id: subDistricts[index].id)),
                         );
                       },
-                      child: DistrictListItem(name: subDistricts[index].zone)
-                    ),
-                  );
-                }
-              }),
-        ),
-      );
-    
+                      child: DistrictListItem(name: subDistricts[index].zone)),
+                );
+              }
+            }),
+      ),
+    );
   }
 }
