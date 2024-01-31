85e28f619edcaf69696694e561026320c6458fc7
Manel-Rejeb <manelrejeb711@gmail.com>
2024-01-31 17:51:00 +0100

    creating the sub-district service

---
+++ lib/service/sub-district.service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:crm/model/sub-district.dart';
+import 'package:http/http.dart' as http;
+
+class SubDistrictService {
+  Uri uri;
+
+  SubDistrictService(id)
+      : uri = Uri.https(
+          "65b2ee529bfb12f6eafe8e75.mockapi.io",
+          "/disctricts/$id/zones",
+        );
+
+  Future<List<SubDistrict>> fetchData() async {
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body);
+      final List<SubDistrict> subDistricts =
+          jsonData.map((item) => SubDistrict.fromMap(item)).toList();
+      return subDistricts;
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+}
