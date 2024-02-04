74302309e594619b5fd13d2ab2a004413c625968
Walee Sebiiy <waleesebiiy@gmail.com>
2024-02-04 14:14:51 +0100

    adding card to display potential clients

--- lib/screen/client-list.dart
+++ lib/screen/client-list.dart
@@ -1,3 +1,5 @@
+import 'package:crm/model/client.dart';
+import 'package:crm/service/client.service.dart';
 import 'package:flutter/material.dart';
 import 'package:crm/screen/client-form.dart';
 import 'package:crm/component/client-floating-add-button.dart';
@@ -18,17 +20,74 @@ class ClientList extends StatefulWidget {
 }
 
 class _ClientListState extends State<ClientList> {
+  late Future<List<Client>> _clientsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _clientsFuture = ClientService().fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('${widget.zone} Clients'),
-      ),
-      floatingActionButton: FloatingButton(
-          path: ClientForm(
-        id: widget.id,
-        code: widget.districtId,
-      )),
-    );
+        appBar: AppBar(
+          title: Text('${widget.zone} Clients'),
+        ),
+        floatingActionButton: FloatingButton(
+            path: ClientForm(
+          id: widget.id,
+          code: widget.districtId,
+        )),
+        body: Container(
+          padding: const EdgeInsets.all(10),
+          width: double.infinity,
+          height: double.infinity,
+          child: FutureBuilder<List<Client>>(
+            future: _clientsFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else {
+                List<Client> client = snapshot.data ?? [];
+                return GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1,
+                    crossAxisSpacing: 4.0,
+                    childAspectRatio: 5 / 1,
+                    mainAxisSpacing: 4.0,
+                  ),
+                  itemCount: client.length,
+                  itemBuilder: (context, index) => GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (context) => ClientForm(
+                                    id: widget.id,
+                                    code: widget.districtId,
+                                  )),
+                        );
+                      },
+                      child: Card(
+                        child: ListTile(
+                          title: Text(
+                              '${client[index].firstName} ${client[index].lastName}'),
+                          subtitle: Text(
+                              "${client[index].house}, ${client[index].street}, ${client[index].city}"),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () {
+                              // delete the client
+                            },
+                          ),
+                        ),
+                      )),
+                );
+              }
+            },
+          ),
+        ));
   }
 }
--- lib/service/client.service.dart
+++ lib/service/client.service.dart
@@ -1,17 +1,22 @@
 import 'dart:convert';
 
+import 'package:crm/model/client.dart';
 import 'package:http/http.dart' as http;
 
 class ClientService {
   Uri uri;
-  ClientService() : uri = Uri.https("", "");
+  ClientService()
+      : uri = Uri.https(
+            "069718fd-da44-4e6b-9797-73c923894358.mock.pstmn.io", "/clients");
 
 // get all clients
-  Future<List<dynamic>> fetchData() async {
+  Future<List<Client>> fetchData() async {
     final response = await http.get(uri);
     if (response.statusCode == 200) {
       final List<dynamic> jsonData = json.decode(response.body);
-      return jsonData;
+      final List<Client> clients =
+          jsonData.map((item) => Client.fromMap(item)).toList();
+      return clients;
     } else {
       throw Exception('Failed to load data');
     }
